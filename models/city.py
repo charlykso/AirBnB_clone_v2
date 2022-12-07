9f58926d81cb7f6bd0b893d962b91ce6222ec149
tony <tony@computer.com>
2022-12-07 22:04:08 +0100

    updated the city
    added alchemy attributes

--- models/city.py
+++ models/city.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel
+from models.base_model import Base
 
 
-class City(BaseModel):
-    """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+class City(BaseModel, Base):
+    """ The city class, contains state ID and name 
+
+        Attributes:
+                __tablename__ (): name of table for class
+                name (sqlalchemy String): name of state
+                state_id (sqlalchemy String): state id
+
+    """
+    __tablename__ = "cities"
+    state_id = Column(String(128), nullable=False)
+    name = Column(String(60), nullable=False, foreign_key=states.id)
