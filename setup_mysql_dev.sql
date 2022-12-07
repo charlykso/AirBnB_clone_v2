e31488a76f506866d14d722d1d266064ea2c1916
Ikenna <ikenna@kali.Ikenna-PC>
2022-12-07 13:13:18 -0500

    correction on MySQL setup dev

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -3,10 +3,8 @@
 CREATE DATABASE IF NO EXISTS hbnb_dev_db;
 -- create a new user
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-USE hbnb_dev_db;
 -- grant previlages to user
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-USE performance_schema;
 -- grant select on performance_schema to user hbnb_dev
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
