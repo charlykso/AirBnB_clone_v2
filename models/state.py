c7b98b29b1653c6a08deaf8553d6189d4b203ab2
tony <tony@computer.com>
2022-12-07 22:04:44 +0100

    updated the state
    added alchemy attributes

--- models/state.py
+++ models/state.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from models.base_model import Base
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """ State class 
+
+        Attributes:
+                    __tablename__ : name of the table
+                    name (sqlalchemy String): name of the state
+
+    """
+    name = Column(String(128), nullable=False)
