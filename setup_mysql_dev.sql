bca08661785bc1b23240c74206c01d51e8403cf5
Ikenna <ikenna@kali.Ikenna-PC>
2022-12-07 12:59:32 -0500

    MySQL setup development

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,13 @@
+-- Prepare MySQL server
+-- Create database
+CREATE DATABASE IF NO EXISTS hbnb_dev_db;
+-- create a new user
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+USE hbnb_dev_db;
+-- grant previlages to user
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+USE performance_schema;
+-- grant select on performance_schema to user hbnb_dev
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
+SET FOREIGN_KEY_CHECKS=1;
--- tests/test_console.py
+++ tests/test_console.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Defines unittests for console.py."""
 import os
-# import pep8
+import pep8
 import unittest
 import models
 from unittest.mock import patch
@@ -279,7 +279,7 @@ class TestHBNBCommand(unittest.TestCase):
                 "** no instance found **\n", f.getvalue())
 
     # def test_destroy(self):
-    #     """Test alternate destroy command inpout"""
+    #     """Test alternate destroy command input"""
     #     with patch('sys.stdout', new=StringIO()) as f:
     #         self.HBNB.onecmd("Galaxy.destroy()")
     #         self.assertEqual(
@@ -289,31 +289,31 @@ class TestHBNBCommand(unittest.TestCase):
     #         self.assertEqual(
     #             "** no instance found **\n", f.getvalue())
 
-    # @unittest.skipIf(type(models.storage) == DBStorage, "Testing DBStorage")
-    # def test_update(self):
-    #     """Test alternate destroy command inpout"""
-    #     with patch('sys.stdout', new=StringIO()) as f:
-    #         self.HBNB.onecmd("sldkfjsl.update()")
-    #         self.assertEqual(
-    #             "** class doesn't exist **\n", f.getvalue())
-    #     with patch('sys.stdout', new=StringIO()) as f:
-    #         self.HBNB.onecmd("User.update(12345)")
-    #         self.assertEqual(
-    #             "** no instance found **\n", f.getvalue())
-    #     with patch('sys.stdout', new=StringIO()) as f:
-    #         self.HBNB.onecmd("create User")
-    #     with patch('sys.stdout', new=StringIO()) as f:
-    #         self.HBNB.onecmd("all User")
-    #         obj = f.getvalue()
-    #     my_id = obj[obj.find('(')+1:obj.find(')')]
-    #     with patch('sys.stdout', new=StringIO()) as f:
-    #         self.HBNB.onecmd("User.update(" + my_id + ")")
-    #         self.assertEqual(
-    #             "** attribute name missing **\n", f.getvalue())
-    #     with patch('sys.stdout', new=StringIO()) as f:
-    #         self.HBNB.onecmd("User.update(" + my_id + ", name)")
-    #         self.assertEqual(
-    #             "** value missing **\n", f.getvalue())
+    @unittest.skipIf(type(models.storage) == DBStorage, "Testing DBStorage")
+    def test_update(self):
+        """Test alternate destroy command inpout"""
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.HBNB.onecmd("sldkfjsl.update()")
+            self.assertEqual(
+                "** class doesn't exist **\n", f.getvalue())
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.HBNB.onecmd("User.update(12345)")
+            self.assertEqual(
+                "** no instance found **\n", f.getvalue())
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.HBNB.onecmd("create User")
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.HBNB.onecmd("all User")
+            obj = f.getvalue()
+        my_id = obj[obj.find('(')+1:obj.find(')')]
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.HBNB.onecmd("User.update(" + my_id + ")")
+            self.assertEqual(
+                "** attribute name missing **\n", f.getvalue())
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.HBNB.onecmd("User.update(" + my_id + ", name)")
+            self.assertEqual(
+                "** value missing **\n", f.getvalue())
 
 
 if __name__ == "__main__":
