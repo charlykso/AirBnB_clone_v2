4ff449aed9f739b84644fa0e111332f38a7733c4
Ikenna <ikenna@kali.Ikenna-PC>
2022-12-08 00:58:16 -0500

    DBStorage - States and Cities

---
+++ models/engine/db_storage.py
@@ -0,0 +1,113 @@
+#!/usr/bin/python3
+"""
+DB Storage
+"""
+
+from os import getenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+from models.base_model import BaseModel, Base
+from models.user import User
+from models.city import City
+from models.state import State
+from models.review import Review
+from models.place import Place
+from models.amenity import Amenity
+
+all_classes = {"City": City,
+               "State": State,
+               "Place": Place,
+               "User": User,
+               "Review": Review,
+               "Amenity": Amenity}
+
+class DBStorage:
+    """
+    Handles Database engine
+    Attributes:
+        __engine: engine for the database
+        __session: session of the database
+    """
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        """
+        Constructor
+        create engine for database
+        """
+
+        self.__engine = create_engine('mysql+msqldb://{}:{}@{}/{}'
+                                      .format(
+                                          getenv('HBNB_MYSQL_USER'),
+                                          getenv('HBNB_MYSQL_PWD'),
+                                          getenv('HBNB_MYSQL_HOST'),
+                                          getenv('HBNB_MYSQL_DB')
+                                          ), pool_pre_ping=True)
+        if getenv('HBNB_ENV') == 'test':
+            Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        """
+        query on the current database session (self.__session) 
+        all objects depending of the class name (argument cls).
+        if cls=None, query all types of objects 
+        (User, State, City, Amenity, Place and Review).
+        this method must return a dictionary: (like FileStorage)
+        """
+
+        return_dict = {}
+        query = []
+
+        if cls:
+            for name, value in all_classes.values():
+                if name == cls:
+                    query = self.__session.query(value)
+                    for obj in query:
+                        key = name + '.' + obj.id
+                        return_dict[key] = obj
+            return return_dict
+        else:
+            for name, value in all_classes.values():
+                query = self.__session.query(value)
+                for obj in query:
+                    key = name + '.' + obj.id
+                    return_dict[key] = obj
+        return return_dict
+
+    def new(self, obj):
+        """
+        add the object to the current database session
+        """
+        self.__session.add(obj)
+
+    def save(self):
+        """
+        commit all changes of the current database session
+        """
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """
+        delete from the current database 
+        session obj if not None
+        """
+        if obj is not None:
+            self.__session.delete(obj)
+
+    def reload(self):
+        """
+        - create all tables in the database (feature of SQLAlchemy)
+        	(WARNING: all classes who inherit from Base must be 
+        	imported before calling Base.metadata.create_all(engine)
+         
+        - create the current database session (self.__session) 
+        from the engine (self.__engine) by using a sessionmaker 
+        - the option expire_on_commit must be set to False ; 
+        and scoped_session - to make sure your Session is thread-safe
+        """
+        Base.metadata.create_all(self.__engine)
+        session_factory = sessionmaker(bind=self.__engine,
+                                       expire_on_commit=False)
+        Session = scoped_session(session_factory)
+        self.__session = Session()
--- models/state.py
+++ models/state.py
@@ -1,8 +1,41 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+
+from os import getenv
+from models.base_model import BaseModel, Base
+from sqlalchemy import String, Column
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """ 
+    State inherits from BaseModel and Base
+    __tablename__ : represents the table name, states
+    name : represents a column containing a string (128 characters)
+           can't be null
+    """
+    
+    __tablename__ = "states"
+    if getenv("HBNB_TYPE_STORAGE") == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", cascade="all, delete", 
+                              backref='state')
+    else:
+        @property
+        def cities(self):
+            """
+            FileStorage relationship between State and City.
+            getter attribute cities that returns 
+            the list of City instances with state_id 
+            equals to the current State.id
+            """
+            from models import storage
+            from models.city import City
+
+            city_list = []
+            cities_dict = storage.all(City)
+
+            for city in cities_dict.values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
