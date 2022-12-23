11c072d10766592851282530d31ec42fe9fcf946
tony <tony@computer.com>
2022-12-23 23:55:46 +0100

    fixed do_create() print out

--- console.py
+++ console.py
@@ -140,7 +140,7 @@ class HBNBCommand(cmd.Cmd):
                     except ValueError:
                         continue
                 new_instance.__dict__[key] = value
-        print(new_instance)
+        print(new_instance.id)
         storage.save()
 
     def help_create(self):
